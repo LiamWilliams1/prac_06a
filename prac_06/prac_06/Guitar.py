9d4e1f0a74eff4bb56eda6a584bdeb2bb03cbd85
LiamWilliams1 <liam.williams@my.jcu.edu.au>
2018-09-17 19:41:22 +1000

    cars

---
+++ prac_06/prac_06/Guitar.py
@@ -0,0 +1,18 @@
+thisyear = 2018
+vintage = 50
+
+
+class Guitar:
+    def __init__(self, name="", year=0, cost=0,):
+       self.name = name
+       self.year = year
+       self.cost = cost
+
+    def __str__(self):
+        return "{} ({}) : ${:,.4f}".format(self.name, self.year, self.cost)
+
+    def get_age(self):
+        return thisyear - self.year
+
+    def is_vintage(self):
+        return self.get_age() >= vintage
